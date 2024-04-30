99f37fe08b1e6be38db018ee1a8a24cfbc1433de
JK-B0T <JK-B0T@users.noreply.github.com>
2024-04-30 12:43:22 +0200

    Update README and create boilerplate files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # library
+
+The purpose of this project is to practice with some objects, constructors adn prototypes.
---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", main, false);
+
+function main () {
+    console.log("Hello World!!!");
+}
