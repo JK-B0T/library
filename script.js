decb508a7973b840277af3e38879fc9d36da6120
JK-B0T <JK-B0T@users.noreply.github.com>
2024-05-02 10:17:35 +0200

    Upgrade dialog style

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ window.addEventListener("DOMContentLoaded", main, false);
 function main () {
     const myLibrary = ["book1", "book2", "book3", "book4", "book5"];
 
+    const dialog = document.querySelector("dialog");
+    const newBookBtn = document.getElementById("newBookBtn");
+    const closeNewBookBtn = document.getElementById("closeNewBookBtn");
+    newBookBtn.addEventListener("click", () => {
+        dialog.showModal();
+    }, false);
+    closeNewBookBtn.addEventListener("click", () => {
+        dialog.close();
+    }, false);
+
+    function manageDialog () {
+        dialog.show()
+    }
+
     function addBookToLibrary (book) {
         myLibrary.push(book);
     }
