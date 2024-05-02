1dbd91a11832d3fd51f61c4c93cb21ff9a209496
JK-B0T <JK-B0T@users.noreply.github.com>
2024-05-02 11:58:09 +0200

    Add new book addition functionality

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 window.addEventListener("DOMContentLoaded", main, false);
 
 function main () {
-    const myLibrary = ["book1", "book2", "book3", "book4", "book5"];
+
+    const book1 = new Book("pop", "popo", 10, "true");
+    const book2 = new Book("pip", "pipi", 32, "false");
+    const book3 = new Book("pep", "pepe", 40, "true");
+    const book4 = new Book("pap", "papa", 52, "false");
+    const myLibrary = [];
+    myLibrary.push(book1);
+    myLibrary.push(book2);
+    myLibrary.push(book3);
+    myLibrary.push(book4);
 
     const dialog = document.querySelector("dialog");
     const newBookBtn = document.getElementById("newBookBtn");
+    const addBookBtn = document.getElementById("addBookBtn");
     const closeNewBookBtn = document.getElementById("closeNewBookBtn");
-    
+    const form = document.querySelector("form");
+
+    function Book (title, autor, pages, isread) {
+        this.title = title;
+        this.autor = autor;
+        this.pages = pages;
+        this.isread = isread;
+    }
+
     newBookBtn.addEventListener("click", () => {
         dialog.showModal();
     }, false);
@@ -14,8 +32,23 @@ function main () {
         dialog.close();
     }, false);
 
-    function addBookToLibrary (book) {
-        myLibrary.push(book);
+    form.addEventListener("submit", addBookToLibrary, false);
+
+    function processForm() {
+        const formData = Object.fromEntries(new FormData(form).entries());
+        return formData;
+    }
+   
+    function addBookToLibrary (event) {
+        event.preventDefault();
+
+        const formData = processForm();
+        const newBook = new Book(formData.title, formData.author, formData.pages_number, formData.is_read);
+        myLibrary.push(newBook);
+        console.log(myLibrary);
+
+        form.reset();
+        dialog.close();
     }
 
     function showLibrary () {
